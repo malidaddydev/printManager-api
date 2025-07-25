bd87a0126d4c2166e514de1da82ee77f7b790cc1
IamAhmedZaki <mahmedzaki670@gmail.com>
2025-07-25 15:49:04 +0500

    product filenamefix

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -14,7 +14,7 @@ exports.createProduct = async (req, res) => {
     // Save uploaded files
     const uploadedFiles = req.files?.map(file => ({
       filename: file.filename,
-      path: `/orderuploads/${file.fileName}`
+      path: `/orderuploads/${file.filename}`
     })) || [];
 
     // Parse colorOptions and sizeQuantities if sent as strings
