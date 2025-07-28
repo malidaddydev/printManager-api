27dda488f5084ff2b701ea02b07fb4c4a3b573f3
IamAhmedZaki <mahmedzaki670@gmail.com>
2025-07-28 19:58:30 +0500

    size route fix

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder,getAllOrders,getProductColors,getSingleOrders,updateOrder,deleteOrder,orderFromToken,getProductsizes } = require('../controllers/order.controller');
+const { createOrder,getAllOrders,getProductColors,getSingleOrders,updateOrder,deleteOrder,orderFromToken,getProductSizes } = require('../controllers/order.controller');
 const upload = require('../middlewares/uploadOrderFiles');
 // const { getAllOrders } = require('../controllers/order.controller');
 // const { getOrder } = require('../controllers/order.controller');
@@ -11,7 +11,7 @@ router.post('/', upload.array('files'), createOrder);
 router.get('/', getAllOrders);
 router.get('/token/:token', orderFromToken);
 router.get('/productColors/:id', getProductColors);
-router.get('/productSizes/:id', getSizeColors);
+router.get('/productSizes/:id', getProductSizes);
 router.get('/:id', getSingleOrders);
 router.put('/:id', updateOrder);
 
