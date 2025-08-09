318a53ba9bd4b9dfcbfb91fa3e8f353fb0c008da
IamAhmedZaki <mahmedzaki670@gmail.com>
2025-08-09 15:05:16 +0500

    back to normal

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -515,7 +515,7 @@ const getAllPaginationOrders = async(req,res)=>{
       
     });
 
-    res.status(200).json({orders,frontendPages,ordersTot});
+    res.status(200).json(orders);
   } catch (error) {
     console.error("Error fetching orders:", error);
     res.status(500).json({ message: 'Internal server error', error: error.message });
